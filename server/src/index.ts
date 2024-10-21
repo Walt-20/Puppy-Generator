afbff15db3f43acf961ff5d470f3d821c60fce86
Walt <walt.c0des20@gmail.com>
2024-10-21 11:25:50 -0600

    committing client server code

---
+++ server/src/index.ts
@@ -0,0 +1,42 @@
+import express, { Request, Response } from 'express';
+import { createClient } from '@supabase/supabase-js';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const supabase = createClient(
+    process.env.PROJECT_URL as string,
+    process.env.API_KEY as string
+);
+
+const BUCKET_NAME = process.env.BUCKET_NAME as string;
+
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://192.168.1.14:3000']
+}));
+app.use(express.json());
+
+const fetchRandomImage = async (req: Request, res: Response): Promise<void> => {
+  console.log('fetchRandomImage');
+  try {
+    const { data } = await supabase
+      .storage
+      .from(BUCKET_NAME)
+      .getPublicUrl('images/alvan-nee-eoqnr8ikwFE-unsplash.jpg')
+
+    res.status(200).json({ imageUrl: data.publicUrl });
+  } catch (error) {
+    console.error('Unexpected error:', error);
+    res.status(500).json({ error: 'Failed to fetch image.' });
+  }
+};
+
+app.get('/api/puppy', fetchRandomImage);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
